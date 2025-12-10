a5de7b8792fd9d983506eb2aadafae5d09e1c722
Maurice Lorenz <me@mauricelorenz.com>
2025-12-10 13:21:47 +0100

    Add ft_putendl_fd.c to libft and refactor header files

--- libft/ft_putendl_fd.c
+++ libft/ft_putendl_fd.c
@@ -1,23 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_putendl_fd.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mlorenz <mlorenz@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/09 12:10:44 by mlorenz           #+#    #+#             */
-/*   Updated: 2025/12/10 13:14:16 by mlorenz          ###   ########.fr       */
+/*   Created: 2025/10/19 21:36:10 by mlorenz           #+#    #+#             */
+/*   Updated: 2025/10/19 22:11:21 by mlorenz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-# include <stdarg.h>
-# include <unistd.h>
+#include "libft.h"
 
-int		ft_atoi(const char *nptr);
-long	ft_atol(const char *nptr);
-int		ft_printf(const char *str, ...);
-int		ft_isdigit(int c);
-void	ft_putendl_fd(char *s, int fd);
-#endif
+void	ft_putendl_fd(char *s, int fd)
+{
+	while (*s)
+	{
+		write(fd, s, 1);
+		s++;
+	}
+	write(fd, "\n", 1);
+}
--- push_swap.h
+++ push_swap.h
@@ -6,18 +6,16 @@
 /*   By: mlorenz <mlorenz@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/02 13:46:27 by mlorenz           #+#    #+#             */
-/*   Updated: 2025/12/04 13:20:49 by mlorenz          ###   ########.fr       */
+/*   Updated: 2025/12/10 13:14:21 by mlorenz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
-# include <stdarg.h>
-# include <unistd.h>
+# include <limits.h>
 # include "libft/libft.h"
 
 int		main(int argc, char **argv);
 int		ft_printf(const char *str, ...);
 int		arguments_valid(int argc, char **argv);
-
 #endif
