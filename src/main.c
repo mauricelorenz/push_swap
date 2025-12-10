277d8eee4ac045a6f0130fdfafe5101f246d09c1
Maurice Lorenz <me@mauricelorenz.com>
2025-12-10 13:28:28 +0100

    Refactor main() to use ft_putendl_fd() instead of write()

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: mlorenz <mlorenz@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/02 14:16:47 by mlorenz           #+#    #+#             */
-/*   Updated: 2025/12/04 15:50:16 by mlorenz          ###   ########.fr       */
+/*   Updated: 2025/12/10 13:13:30 by mlorenz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,6 @@ int	main(int argc, char **argv)
 	if (argc < 2)
 		return (1);
 	if (!arguments_valid(argc, argv))
-		return (write(2, "Error\n", 6), 2);
-	else
-		ft_printf("all args valid!\n");
-	ft_printf("%s %s %s\n", argv[1], argv[2], argv[3]);
+		return (ft_putendl_fd("Error", 2), 2);
 	return (0);
 }
