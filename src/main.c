2fb7e83ccc3191c1e76f6e73efa1d9630367d5a0
Maurice Lorenz <me@mauricelorenz.com>
2025-12-18 22:18:52 +0100

    Add sort.c with sort_stacks()

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: mlorenz <mlorenz@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/02 14:16:47 by mlorenz           #+#    #+#             */
-/*   Updated: 2025/12/16 23:22:31 by mlorenz          ###   ########.fr       */
+/*   Updated: 2025/12/18 21:54:13 by mlorenz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 int	main(int argc, char **argv)
 {
 	t_stack	*stack_a;
-	t_stack	*stack_a_orig;
+	t_stack	*stack_b;
 
 	if (argc < 2)
 		return (1);
@@ -26,8 +26,10 @@ int	main(int argc, char **argv)
 	stack_a = populate_stack(argc, argv);
 	if (!stack_a)
 		return (3);
-	stack_a_orig = stack_a;
-	stack_clear(&stack_a_orig);
+	stack_b = NULL;
+	sort_stacks(&stack_a, &stack_b);
+	stack_clear(&stack_a);
+	stack_clear(&stack_b);
 	return (0);
 }
 
@@ -92,20 +94,30 @@ int	main(int argc, char **argv)
 // 	stack_b = NULL;
 // 	if (!stack_a)
 // 		return (3);
+// 	ft_printf("stack_a: %p\n", stack_a);
 // 	test_output(stack_a);
 // 	ft_putendl_fd("---", 1);
-// 	test_output(stack_b);
-// 	pab(&stack_a, &stack_b, 'a');
-// 	test_output(stack_a);
-// 	ft_putendl_fd("---", 1);
+// 	ft_printf("stack_b: %p\n", stack_b);
 // 	test_output(stack_b);
 // 	pab(&stack_b, &stack_a, 'b');
+// 	pab(&stack_b, &stack_a, 'b');
+// 	pab(&stack_b, &stack_a, 'b');
+// 	pab(&stack_b, &stack_a, 'b');
+// 	pab(&stack_b, &stack_a, 'b');
+// 	ft_printf("stack_a: %p\n", stack_a);
 // 	test_output(stack_a);
 // 	ft_putendl_fd("---", 1);
+// 	ft_printf("stack_b: %p\n", stack_b);
 // 	test_output(stack_b);
 // 	pab(&stack_a, &stack_b, 'a');
+// 	pab(&stack_a, &stack_b, 'a');
+// 	pab(&stack_a, &stack_b, 'a');
+// 	pab(&stack_a, &stack_b, 'a');
+// 	pab(&stack_a, &stack_b, 'a');
+// 	ft_printf("stack_a: %p\n", stack_a);
 // 	test_output(stack_a);
 // 	ft_putendl_fd("---", 1);
+// 	ft_printf("stack_b: %p\n", stack_b);
 // 	test_output(stack_b);
 // 	stack_clear(&stack_a);
 // 	stack_clear(&stack_b);
